7aa401616f6e10d18d80b3ca62f661fefa4df764
Jens Rosseau <jens.rosseau@gmail.com>
2019-03-25 15:27:48 +0100

    Spring BlockChain edited controller
    Added mapping for controller to transaction form

--- Spring-BlockChain/src/main/java/edu/ap/spring/service/BlockchainController.java
+++ Spring-BlockChain/src/main/java/edu/ap/spring/service/BlockchainController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import edu.ap.spring.grade.jpa.Grade;
+import edu.ap.spring.transaction.Transaction;
+
 @Controller
 public class BlockchainController {
 	
@@ -12,7 +15,13 @@ public class BlockchainController {
 		
 	}
 	
-	@RequestMapping("/balance/walletA")
+	@Autowired
+	private Transaction transaction;
+	private Wallet wallet;
+	private Block block;
+	private BlockChain blockChain;
+	
+	@GetMapping("/balance/walletA")
 	   public String balance( Model model) {
 		Wallet walletA;
 		walletA.generateKeyPair();
@@ -21,7 +30,23 @@ public class BlockchainController {
 		return "balance";
 	   }
 	
-	@RequestMapping("/")
+	@GetMapping("/transaction")
+		public String transaction() {
+			return "transaction";
+	}
+	
+	@PostMapping("/transaction")
+	   //worden volgende waarde opgehaald uit dit form
+	   public String setGrade(@RequestParam("receiver") PublicKey receiver,
+	                          @RequestParam("value") float value) {
+		  //vervolgens worden al deze waarde opgeslagen in de repository als een nieuw grade object
+			wallet.sendFunds(receiver, value);
+	      // daarna geredirect naar de list methode
+	      return "redirect:/";
+	   }
+	
+	
+	@GetMapping("/")
 	   public String root() {
 		   return "redirect:/balance/walletA";
 	   }
