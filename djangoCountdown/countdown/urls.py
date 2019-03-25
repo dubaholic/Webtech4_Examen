123412967877cf85676940d8e411ae2a38141713
Jens Rosseau <jens.rosseau@gmail.com>
2019-03-25 14:49:24 +0100

    Django countdown working
    Countdown till brexit working version

--- djangoCountdown/countdown/urls.py
+++ djangoCountdown/countdown/urls.py
@@ -2,11 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = 'countdown'
-#als de gebruiker naar 'quotes/' 1 van volgende zaken surft dan worden deze urls aangeroepen
+
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('search/quotes', views.search_quotes, name='search_quotes'),
-	path('search/form', views.search_form, name='search_form'),
-	#hierbij wordt ook de author_name meegegeven met de methode van de view
-	path('<str:author_name>/', views.detail, name='detail'),
-]
+	]
--- djangoCountdown/countdown/views.py
+++ djangoCountdown/countdown/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+import datetime
+from datetime import timedelta
 
 # Create your views here.
+
+def index(request):
+    timeLocal = datetime.datetime.now()
+    timeBrexit = datetime.datetime(2019, 3, 29, 23,00,00)
+    diff = timeBrexit - timeLocal
+    days, seconds = diff.days, diff.seconds
+    hours = days * 24 + seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = seconds % 60
+    print(hours)
+    
+    print(timeBrexit)
+    print(timeLocal)
+    return render(request, 'countdown/index.html', {'hours': hours, 'minutes': minutes, 'seconds': seconds})
