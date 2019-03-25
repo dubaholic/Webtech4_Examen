6c53be882336b6465bb857b5fc64dca80a8f18cc
Jens Rosseau <jens.rosseau@gmail.com>
2019-03-25 14:07:22 +0100

    Django starter project added

---
+++ djangoCountdown/djangoCountdown/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for djangoCountdown project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoCountdown.settings')
+
+application = get_wsgi_application()
