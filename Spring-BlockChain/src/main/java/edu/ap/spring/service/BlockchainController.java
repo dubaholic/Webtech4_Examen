c8fa2af2068d8f5708e7118fa68a1aedd9f4572d
Jens Rosseau <jens.rosseau@gmail.com>
2019-03-25 15:17:38 +0100

    Spring Blockchain edited controller
    Edited the controller a little bit

--- Spring-BlockChain/src/main/java/edu/ap/spring/service/BlockchainController.java
+++ Spring-BlockChain/src/main/java/edu/ap/spring/service/BlockchainController.java
@@ -1,17 +1,28 @@
 package edu.ap.spring.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class BlockchainController {
-
+	
 	public BlockchainController() {
 		
 	}
 	
-	@RequestMapping("/balance")
-	   public String balance() {
-		   return "Balance";
+	@RequestMapping("/balance/walletA")
+	   public String balance( Model model) {
+		Wallet walletA;
+		walletA.generateKeyPair();
+		
+		 model.addAttribute("wallet", walletA.getBalance());
+		return "balance";
+	   }
+	
+	@RequestMapping("/")
+	   public String root() {
+		   return "redirect:/balance/walletA";
 	   }
 }
